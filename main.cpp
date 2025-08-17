d2c8f9b5af31683b5fc39fa42c80c62bed094320
SlashRblx <mikehaknpc@gmail.com>
2025-08-17 12:43:34 +0300

    it real

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void exitscam() {
+  database.delte();
+}
+
+void crash() {
+  virus.pc.crash();
+}
+
+int main() {
+  exitscam();
+  crash();
+  std::cout << "Let's cut to the chase: we will no longer continue offering Synapse X. We have shut down the software's services as of this announcement, and all user data has been deleted.
+As important Synapse X was for the growth and credence of our company, it became clear that we must expand our horizons and move on. In light of this new direction, we have decided to sunset Synapse X and begin work on more ambitious projects. We are currently working on many very exciting things, which we hope to showcase soon. For now, you can learn more about our recent undertaking here: https://devforum.roblox.com/t/exploit-prevention-update/2663101
+We want to thank our users for their support and understanding throughout the years. It really has been a pleasure to serve you all. We are thrilled about what the future holds for us, and we hope that you will continue to follow our journey. Sincerely The Synapse team.";
+  return 0;
+}
